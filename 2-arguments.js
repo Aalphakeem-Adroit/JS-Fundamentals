14de63c0f84770a2e2671b2fc32c1003896eac2f
Aalphakeem-Adroit <alphakeem12@gmail.com>
2025-10-01 17:31:58 +0100

    Fix: Argument Syntax

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,12 +1,23 @@
-function arg(a, b) {
-    if (arguments.length < 1) {
-        console.log("No argument");
-} else if (arguments.length === 1) {
-        console.log("Argument found");
-} else {
-        console.log("Arguments found");
-}}
+// function arg(a, b) {
+//     if (arguments.length < 1) {
+//         console.log("No argument");
+// } else if (arguments.length === 1) {
+//         console.log("Argument found");
+// } else {
+//         console.log("Arguments found");
+// }}
 
-arg();
-arg(1);
-arg(1, 2, 3);
+// arg();
+// arg(1);
+// arg(1, 2, 3);
+
+const args = process.argv.length - 2;
+
+
+if (args < 1) {
+    console.log("No argument");
+} else if (args === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
