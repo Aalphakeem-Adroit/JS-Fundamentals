94ac659392fc894811ba005225613eda1c8d2a44
Aalphakeem-Adroit <alphakeem12@gmail.com>
2025-10-01 17:41:35 +0100

    Creating a sentence with concatenation

--- 4-concat.js
+++ 4-concat.js
@@ -1,4 +1,8 @@
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
+// const arg1 = process.argv[2];
+// const arg2 = process.argv[3];
 
-console.log(arg1 + " is " + arg2);
+// console.log(arg1 + " is " + arg2);
+
+const args = process.argv.length - 2;
+
+console.log(process.argv[2] + " is " + process.argv[3]);
